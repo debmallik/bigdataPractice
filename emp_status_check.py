e005b7a60882fdce4a2e6c140e5757d1e5e61e10
debmallik <debmallik.29@gmail.com>
2024-11-03 18:46:55 +0530

    added init functionality

--- emp_status_check.py
+++ emp_status_check.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 import os
 
-from pyspark.sql.functions import datediff, current_date, when
+from pyspark.sql.functions import datediff, current_date, when, initcap
 
 os.environ["PYSPARK_PYTHON"]="C:/Users/hp/Desktop/bigdata/Python/Python/Python37/python.exe"
 
@@ -34,5 +34,6 @@ employees_df=spark.createDataFrame(employees,["name","last_checkin"])
 checkin_duration_df=employees_df.withColumn("checkin_duration",datediff(current_date(),"last_checkin"))
 
 status_df=checkin_duration_df.withColumn("status",when(checkin_duration_df["checkin_duration"]<7,"active").otherwise("Inactive"))
+status_df=status_df.withColumn("name",initcap("name"))
 
 status_df.show()
